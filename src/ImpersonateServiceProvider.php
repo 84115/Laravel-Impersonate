f5be7c1702074eb82fb05cd08b3586a5045c2448
James <jamesballsemail@gmail.com>
2023-03-21 19:22:39 +0000

    Cleanup Service Provider

--- src/ImpersonateServiceProvider.php
+++ src/ImpersonateServiceProvider.php
@@ -6,24 +6,17 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
 use J84115\Impersonate\Http\Controllers\ImpersonateController;
 
-class STANDARD {}
-
 class ImpersonateServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(STANDARD::class, function ($app) {
-            return new STANDARD;
-        });
+        // 
     }
  
     public function boot(Router $router)
     {
         $router->middlewareGroup('impersonate', [ImpersonateMiddleware::class]);
 
-
-        // TODO: tommorow
-        // $router = $this->app['router'];
         $router->macro('impersonate', function () use ($router) {
             $router->get(
                 '/impersonate/login/{uid}',
@@ -34,22 +27,6 @@ class ImpersonateServiceProvider extends ServiceProvider
                 '/impersonate/logout',
                 [ImpersonateController::class, 'logout']
             )->name('impersonate.logout');
-            // dd(1);
         });
-
-
-
-        // if (file_exists($middleware = __DIR__.'/middleware.php')) {
-        //     require $middleware;
-        // }
-
-        // if (file_exists($controller = __DIR__.'/controller.php')) {
-        //     require $controller;
-        // }
     }
-
-    // public function provides()
-    // {
-    //     return [STANDARD::class];
-    // }
 }
